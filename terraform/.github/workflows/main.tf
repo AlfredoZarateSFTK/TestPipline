34579a35ee3bde770125e469cd5ba8b87e07cf6e
AlfredoZarateSFTK <jose.rodriguezz@softtek.com>
2026-02-18 17:20:00 +0000

    Desarrollo inicial Pipeline

---
+++ terraform/.github/workflows/main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "bad_bucket" {
+  bucket = "poc-checkov-bucket-123456"
+
+  acl = "public-read"
+}
+
+resource "aws_security_group" "bad_sg" {
+  name        = "allow_all"
+  description = "Allow all inbound traffic"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
